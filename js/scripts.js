6258daaf280c9b8cfb3b9dec28abea2052fe08ac
Christine Frank and Ethan Samuels-Ellingson <ethan@>
2019-08-14 10:33:34 -0700

    change user input to form instead of prompt.

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,12 @@ var divide = function(number1, number2) {
   return number1 / number2;
 }
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter a second number"));
+$(document).ready(function(){
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
 
-alert(divide(number1, number2));
+});
